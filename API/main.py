15adc1807e6e5c15a19f391e8807ad7beb1effec
Rəsul <71267982+ResuIl@users.noreply.github.com>
2022-11-18 21:32:41 +0400

    Bug Fix 2

--- API/main.py
+++ API/main.py
@@ -63,6 +63,15 @@ def GetUser(Username: str, Password: str):
 
             LastUser = Cursor.fetchall()[0]
             return LastUser
+    except KeyError:
+        try:
+            Cursor.execute(f"SELECT * FROM users WHERE Username = '{Username}' AND Password = '{Password}'")
+            Connection.commit()
+
+            LastUser = Cursor.fetchall()[0]
+            return LastUser
+        except Exception:
+            return []
     except Exception:
         return []
         
