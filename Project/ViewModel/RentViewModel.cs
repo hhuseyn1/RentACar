78adcb6cf0a14f8a681438a7eb4818adcded2b08
Hemi_bk15 <Hemi_bk15@STHQ0124-06>
2022-12-02 12:35:15 +0400

    Rent Relay Command

--- Project/ViewModel/RentViewModel.cs
+++ Project/ViewModel/RentViewModel.cs
@@ -1,15 +1,23 @@
 ﻿using Project.Repositories;
 using System;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Project.ViewModel;
 
 class RentViewModel : ViewModelBase
 {
+    public ICommand RentBtn { get; }
 
     public RentViewModel()
     {
+        RentBtn = new RelayCommand(ExecuteRentCommand);
 
+    }
+
+    private void ExecuteRentCommand(object obj)
+    {
+        MessageBox.Show("Test");
     }
 }
